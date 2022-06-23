f67f2d8aba353d26dad6acbe0f22bb5743c12149
Shweta_Kodam-at-992512424612 <shweta.kodam@evolutionco.com>
2022-06-23 23:07:19 +0530

    js

---
+++ app1.js
@@ -0,0 +1,62 @@
+
+// Assignment questions here
+
+// ------------Qst 1-------------
+
+// Print factorial of a number using recursion
+
+// function y(num,fact){
+//   if(num>0){
+//     fact *= num;
+//     return y(num-1 ,fact);
+//   }
+//   return fact;
+// }
+
+// console.log(y(5,1));
+
+
+// -----------Qst 2--------------
+
+// Given a number n, print numbers from 1 to n using recursion.
+
+
+// function range(num, num2){
+//     for(i = num ; i <= num2; i++ ){
+//         console.log(i);
+//         return range(num+1, num2 );
+//     }
+// }
+// console.log( range(1,7) );
+
+
+// ------------Qst 3-----------
+
+// Given a number n, print numbers from n to 1 using recursion.
+
+// function range(num, num2) {
+//   if (num >= num2) {
+//     console.log(num);
+//     return range(num - 1, num2);
+//   }
+
+// }
+// console.log(range(5, 1));
+
+
+
+// ------------------Qsst 4---------------
+
+// Print ‘n’ numbers of Fibonacci series using recursion.
+
+
+// function fibo(first, second, counter) {
+//     if (counter <=10) {
+//         var c = first + second;
+//         console.log(first);
+//         return fibo(second, c, counter + 1);
+
+//     }
+// }
+// fibo(1, 1, 1);
+
